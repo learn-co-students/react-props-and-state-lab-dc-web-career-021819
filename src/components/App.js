b5d42b9d7b77eb4ef235d3aa9fc074e8eeacff76
stephenyoon84 <stephenyoon84@gmail.com>
2019-04-24 14:41:17 -0400

    add solution

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,45 @@ class App extends React.Component {
     }
   }
 
+  changeType = e => {
+    this.setState({
+      // ...this.state,
+      filters: {
+        type: e.target.value
+      }
+    })
+  }
+
+  findPets = e => {
+    const targetType = this.state.filters.type
+    // console.log(targetType)
+    // console.log(this.state.pets)
+    const url = targetType === 'all' ? '/api/pets' : `/api/pets?type=${targetType}`
+    // console.log(url)
+    fetch(url)
+      .then(r => r.json())
+      .then(d => this.setState({ pets: d}))
+    // if (targetType === 'all') {
+    //   console.log('Hi')
+    //   fetch('/api/pets')
+    //     .then(r => r.json())
+    //     .then(d => this.changePets(d))
+    // } else {
+    //   console.log('hello')
+    //   fetch(`/api/pets?type=${targetType}`)
+    //     .then(r => r.json())
+    //     .then(d => console.log(d))
+    // }
+    // console.log(this.state.pets)
+  }
+
+  onAdoptPet = petId => {
+    const pets = this.state.pets.map(p => {
+      return p.id === petId ? {...p, isAdopted: true} : p
+    })
+    this.setState({pets})
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +63,13 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters
+                onChangeType={this.changeType}
+                onFindPetsClick={this.findPets}
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
